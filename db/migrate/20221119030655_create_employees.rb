8f87e0d9eabc2d0648fc1057f8b06e6cf939eab5
Erik Cardenas <efdree@gmail.com>
2022-11-21 22:29:23 -0500

    First

---
+++ db/migrate/20221119030655_create_employees.rb
@@ -0,0 +1,16 @@
+class CreateEmployees < ActiveRecord::Migration[7.0]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :nationality
+      t.string :email
+      t.string :position
+      t.references :department, null: false, foreign_key: true
+      t.date :birth_date
+      t.string :manager
+      t.integer :role
+
+      t.timestamps
+    end
+  end
+end
